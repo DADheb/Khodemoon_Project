5dbc87a021de25253370f1aba542ebc2aeb7eb85
tinahalimi <106162910+tinahalimi@users.noreply.github.com>
2022-08-06 15:18:48 +0430

    Delete demoMyTwitter directory

--- demoMyTwitter/src/main/java/com/example/demomytwitter/PostController.java
+++
@@ -1,4 +0,0 @@
-package com.example.demomytwitter;
-
-public class PostController {
-}
--- demoMyTwitter/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demomytwitter {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.demomytwitter to javafx.fxml;
-    exports com.example.demomytwitter;
-}
